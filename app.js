b9843d5d3d26ed95623486888d8fda32f1bcb835
muhammad ubaidilah <mubay30@gmail.com>
2025-06-17 06:16:44 +0700

    added create, edit and delete features

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
@@ -17,6 +18,10 @@ mongoose.connect('mongodb://127.0.0.1/spotwise')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -26,9 +31,34 @@ app.get('/places', async (req, res) => {
     res.render('places/index', { places });    
 })
 
+app.get('/places/create', (req, res) => {
+    res.render('places/create');
+})
+
+app.post('/places', async (req, res) => {
+    const place = new Place(req.body.place);
+    await place.save();
+    res.redirect('/places');
+})
+
 app.get('/places/:id', async (req, res) => {
     const place = await Place.findById(req.params.id);
     res.render('places/show', { place });
+})
+
+app.get('/places/:id/edit', async (req, res) => {
+    const place = await Place.findById(req.params.id);
+    res.render('places/edit', { place });
+})
+
+app.put('/places/:id', async (req, res) => {
+    await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
+    res.redirect('/places');
+})
+
+app.delete('/places/:id', async (req, res) => {
+    await Place.findByIdAndDelete(req.params.id);
+    res.redirect('/places');
 })
 
 // app.get('/seed/place', async (req, res) => {
