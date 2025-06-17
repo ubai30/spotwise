b2dce80b5f42a5a60edf5abe2232942cb82514bb
muhammad ubaidilah <mubay30@gmail.com>
2025-06-18 06:26:28 +0700

    index place page styling

--- seeds/place.js
+++ seeds/place.js
@@ -16,77 +16,77 @@ async function seedPlaces() {
             price: 20000,
             description: 'Taman hiburan keluarga dengan berbagai replika bangunan dari seluruh Indonesia',
             location: 'Taman Mini Indonesia Indah, Jakarta',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Pantai Kuta',
             price: 0,
             description: 'Pantai yang terkenal di Bali dengan pemandangan sunset yang indah',
             location: 'Pantai Kuta, Kuta, Badung Regency, Bali',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Borobudur',
             price: 0,
             description: 'Candi Buddha terbesar di dunia yang terletak di Magelang, Jawa Tengah',
             location: 'Borobudur, Magelang, Central Java',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Kawah Putih',
             price: 0,
             description: 'Kawah vulkanik dengan danau berwarna putih di Bandung, Jawa Barat',
             location: 'Kawah Putih, Ciwidey, West Java',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Malioboro',
             price: 0,
             description: 'Jalan utama di Yogyakarta dengan berbagai toko dan kuliner khas',
             location: 'Jl. Malioboro, Yogyakarta City, Special Region of Yogyakarta',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Pantai Tanjung Aan',
             price: 10000,
             description: 'Pantai dengan pasir berwarna putih dan air laut yang jernih di Lombok, Nusa Tenggara Barat',
             location: 'Pantai Tanjung Aan, Lombok, West Nusa Tenggara',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Bukit Bintang',
             price: 0,
             description: 'Kawasan perbelanjaan dan hiburan di Kuala Lumpur, Malaysia',
             location: 'Bukit Bintang, Kuala Lumpur, Federal Territory of Kuala Lumpur, Malaysia',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Candi Prambanan',
             price: 25000,
             description: 'Candi Hindu terbesar di Indonesia yang terletak di Yogyakarta',
             location: 'Candi Prambanan, Sleman, Special Region of Yogyakarta',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Danau Toba',
             price: 0,
             description: 'Danau vulkanik terbesar di Indonesia yang terletak di Sumatera Utara',
             location: 'Danau Toba, North Sumatra',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Kawah Ijen',
             price: 100000,
             description: 'Kawah vulkanik dengan fenomena blue fire di Banyuwangi, Jawa Timur',
             location: 'Kawah Ijen, Banyuwangi, East Java',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Pantai Sanur',
             price: 0,
             description: 'Pantai di Bali yang cocok untuk berenang dan melihat matahari terbit',
             location: 'Pantai Sanur, Denpasar, Bali',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
 
         {
@@ -94,63 +94,63 @@ async function seedPlaces() {
             price: 25000,
             description: 'Candi Buddha terbesar di dunia yang terletak di Magelang, Jawa Tengah',
             location: 'Candi Borobudur, Borobudur, Magelang, Central Java',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Pulau Komodo',
             price: 5000000,
             description: 'Pulau di Indonesia yang terkenal dengan komodo, hewan terbesar di dunia',
             location: 'Pulau Komodo, East Nusa Tenggara',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Taman Nasional Gunung Rinjani',
             price: 150000,
             description: 'Taman nasional yang terletak di Lombok dan memiliki gunung tertinggi kedua di Indonesia',
             location: 'Taman Nasional Gunung Rinjani, Lombok, West Nusa Tenggara',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Bukit Tinggi',
             price: 0,
             description: 'Kota kecil yang terletak di Sumatera Barat dengan arsitektur khas Eropa',
             location: 'Bukit Tinggi, West Sumatra',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Pulau Weh',
             price: 0,
             description: 'Pulau yang terletak di ujung barat Indonesia dengan keindahan bawah laut yang luar biasa',
             location: 'Pulau Weh, Sabang, Aceh',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Taman Safari Indonesia',
             price: 0,
             description: 'Taman hiburan keluarga dengan berbagai satwa liar di Cisarua, Bogor',
             location: 'Taman Safari Indonesia, Cisarua, West Java',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Gunung Merbabu',
             price: 50000,
             description: 'Gunung yang terletak di Jawa Tengah dengan pemandangan matahari terbit yang indah',
             location: 'Gunung Merbabu, Central Java',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Pulau Lombok',
             price: 0,
             description: 'Pulau di Indonesia yang terkenal dengan keindahan pantainya',
             location: 'Pulau Lombok, West Nusa Tenggara',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
             title: 'Tanjung Lesung',
             price: 100000,
             description: 'Kawasan wisata pantai di Banten yang cocok untuk bersantai dan berenang',
             location: 'Tanjung Lesung, Pandeglang, Banten',
-            image: 'https://source.unsplash.com/collection/2349781/1280x720'
+            image: 'https://images.unsplash.com/photo-1442544213729-6a15f1611937?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         }
     ]
 
