69409809a98472688b15ad2123541b8b06d6224f
muhammad ubaidilah <mubay30@gmail.com>
2025-06-21 07:26:14 +0700

    Delete all reviews when a place is deleted

--- models/place.js
+++ models/place.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const Review = require('./review')
 
 const placeSchema = new Schema({
     title: String,
@@ -11,6 +12,12 @@ const placeSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Review'
     }]
+})
+
+placeSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({ _id: { $in: doc.reviews }})
+    }
 })
 
 module.exports = mongoose.model('Place', placeSchema)
