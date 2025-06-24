927336ac99a4f04e903d759ff8f2ad3bebcd75d6
muhammad ubaidilah <mubay30@gmail.com>
2025-06-25 06:27:18 +0700

    create auto login flow after register

--- routes/auth.js
+++ routes/auth.js
@@ -12,9 +12,12 @@ router.post('/register', wrapAsync(async (req, res) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
-        await User.register(user, password);
-        req.flash('success_msg', 'You are registered and can log in');
-        res.redirect('/login');
+        const registerUser = await User.register(user, password);
+        req.login(registerUser, (err) => {
+            if (err) return next();
+            req.flash('success_msg', 'You are registered and can logged in');
+            res.redirect('/places');
+        })
     } catch (error) {
         req.flash('error_msg', error.message);
         res.redirect('/register');
