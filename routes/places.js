f87ef3f2d877460069428d0c3ec36034a835d317
muhammad ubaidilah <mubay30@gmail.com>
2025-06-24 07:16:35 +0700

    create auth middleware to check user

--- routes/places.js
+++ routes/places.js
@@ -4,6 +4,7 @@ const { placeSchema } = require('../schemas/place');
 const wrapAsync = require('../utils/wrapAsync');
 const ExpressError = require('../utils/ExpressError');
 const isValidObjectId = require('../middlewares/isValidObjectId');
+const isAuth = require('../middlewares/isAuth');
 
 const router = express.Router();
 
@@ -22,11 +23,11 @@ router.get('/', wrapAsync(async(req, res) => {
     res.render('places/index', { places });    
 }))
 
-router.get('/create', (req, res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('places/create');
 })
 
-router.post('/', validatePlace, wrapAsync(async (req, res, next) => {
+router.post('/', isAuth, validatePlace, wrapAsync(async (req, res, next) => {
     const place = new Place(req.body.place);
     await place.save();
     req.flash('success_msg', 'Place added successfully');
@@ -38,18 +39,18 @@ router.get('/:id', isValidObjectId('/places'), wrapAsync(async (req, res) => {
     res.render('places/show', { place });
 }))
 
-router.get('/:id/edit', isValidObjectId('/places'), wrapAsync(async (req, res) => {
+router.get('/:id/edit', isAuth, isValidObjectId('/places'), wrapAsync(async (req, res) => {
     const place = await Place.findById(req.params.id);
     res.render('places/edit', { place });
 }))
 
-router.put('/:id', isValidObjectId('/places'), validatePlace, wrapAsync(async (req, res) => {
+router.put('/:id', isAuth, isValidObjectId('/places'), validatePlace, wrapAsync(async (req, res) => {
     await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
     req.flash('success_msg', 'Place updated successfully');
     res.redirect(`/places/${req.params.id}`);
 }))
 
-router.delete('/:id', isValidObjectId('/places'), wrapAsync(async (req, res) => {
+router.delete('/:id', isAuth, isValidObjectId('/places'), wrapAsync(async (req, res) => {
     await Place.findByIdAndDelete(req.params.id);
     req.flash('success_msg', 'Place deleted successfully');
     res.redirect('/places');
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,7 @@ const Place = require('../models/place');
 const wrapAsync = require('../utils/wrapAsync');
 const ExpressError = require('../utils/ExpressError');
 const isValidObjectId = require('../middlewares/isValidObjectId');
+const isAuth = require('../middlewares/isAuth');
 
 const router = express.Router( {mergeParams: true} );
 
@@ -22,7 +23,7 @@ const validateReview = (req, res, next) => {
     }
 }
 
-router.post('/', isValidObjectId('/places'), validateReview, wrapAsync(async (req, res) => {
+router.post('/', isAuth, isValidObjectId('/places'), validateReview, wrapAsync(async (req, res) => {
     const review = new Review(req.body.review);
     const place = await Place.findById(req.params.place_id);
     place.reviews.push(review);
@@ -32,7 +33,7 @@ router.post('/', isValidObjectId('/places'), validateReview, wrapAsync(async (re
     res.redirect(`/places/${req.params.place_id}`);
 }))
 
-router.delete('/:review_id', isValidObjectId('/places'), wrapAsync(async (req, res) => {
+router.delete('/:review_id', isAuth, isValidObjectId('/places'), wrapAsync(async (req, res) => {
     const { place_id, review_id } = req.params;
     await Place.findByIdAndUpdate(place_id, { $pull: { reviews: review_id }});
     await Review.findByIdAndDelete(review_id);
