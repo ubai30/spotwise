d79afceed3dab990fd44649b0ea3797fa7899b5d
muhammad ubaidilah <mubay30@gmail.com>
2025-06-28 06:38:11 +0700

    multer setup and file upload example

---
+++ configs/multer.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+const path = require('path');
+const ExpressError = require('../utils/ExpressError')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/');
+    },
+
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() *1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true)
+        } else {
+            cb(new ExpressError('Only images are allowed.', 405 ));
+        }
+    }
+});
+
+module.exports = upload;
